06016085424f5cf61475b837d4928e1830b9b942
maher elsayed <maher.elsayed.dev@gmail.com>
2020-10-12 04:08:01 +0200

    change path in server get route

--- src/server/app.js
+++ src/server/app.js
@@ -19,8 +19,8 @@ app.use(cors());
 app.use(express.static("dist"));
 
 app.get("/", function (req, res) {
-    //res.sendFile('dist/index.html')
-    res.sendFile(path.resolve("src/client/views/index.html"));
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve("src/client/views/index.html"));
 });
 
 let projectData = {};
